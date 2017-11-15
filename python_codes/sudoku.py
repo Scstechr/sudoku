feb65c485f7f1555a2302f345936b05fadd8d0b1
Scstechr <teufelkonig@gmail.com>
2017-11-16 01:12:18 +0900

    pc: input done

--- python_codes/sudoku.py
+++ python_codes/sudoku.py
@@ -2,60 +2,45 @@ import sys
 import os
 
 def main():
+    # changing directry and question based on input
     path = sys.argv[1]
     filename = '{}/{}'.format(sys.argv[1],sys.argv[2])
     os.chdir(path)
-    userlist = []
-    with open(filename, 'r') as f:
-        for line in f:
-            string = line.split()
-            print(string)
 
-    #     for line in f:
-    #         user = {}
-    #         userid = line[:line.find(',')]
-    #         username = line[line.find(',')+1:].replace('\n','')
-    #         name = '{},{}'.format(userid,username)
-    #         user['id'] = userid;
-    #         user['name'] = username;
-    #         userlist.append(user)
-    #         user['size'] = 0;
-    #         user['filenum'] = 0;
-    #         # print(line)
-    #
-    # for line in userlist:
-    #     print(line)
-    #
-    # with open('files_list.txt', 'r') as f:
-    #     for line in f:
-    #         if line.find('U') != 0:
-    #             userid = line[line.find('U'):]
-    #             userid = userid[:userid.find(',')]
-    #             size = line[line.find('U'):]
-    #             size = size[size.find(',')+1:]
-    #             size = size[size.find(',')+1:]
-    #             size = size[:size.find(',')]
-    #             for user in userlist:
-    #                 if user['id'] == userid:
-    #                     user['size'] += int(size)
-    #                     user['filenum'] += 1
-    #                     break
-    #
-    # print("\n")
-    #
-    # totalsize = 0
-    # for line in userlist:
-    #     size = line['size']
-    #     totalsize += int(size)
-    #     print(totalsize)
-    #
-    # for line in userlist:
-    #     size = line['size']
-    #     line['size'] /= totalsize
-    #
-    # for line in userlist:
-    #     print(line)
-    #
-    # print(len('r.takahashi'))
+    #input
+    with open(filename, 'r') as f:
+        sudoku = []
+
+        check = {}      # empty
+        check['r'] = [] # rows
+        check['c'] = [] # columns
+        check['s'] = [] # squares
+        for i in range(9):
+            column = set()
+            square = set()
+            check['c'].append(column)
+            check['s'].append(square)
+        row_count = 0
+        for line in f:
+            if row_count == 0:
+                offset = 0
+            elif row_count > 0 and row_count%3 == 0:
+                offset += 1
+            string = line.split()
+            row = set()
+            for num in string:
+                if num != '0':
+                    row.update(num)
+                    check['c'][string.index(num)].update(num)
+                    if string.index(num) < 3:
+                        check['s'][3*offset].update(num)
+                    elif string.index(num) < 6:
+                        check['s'][3*offset+1].update(num)
+                    else:
+                        check['s'][3*offset+2].update(num)
+            check['r'].append(row)
+            sudoku.append(string)
+            row_count += 1
+
 if __name__ == '__main__':
     main()
